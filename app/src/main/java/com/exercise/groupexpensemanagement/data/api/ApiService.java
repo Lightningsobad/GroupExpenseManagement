a152ad6abdea1677abe9bf4998fcaba43852f1e1
lightningsobad_ <phamquangk993@gmail.com>
2025-12-14 14:04:03 +0700

    add-primary-features

--- app/src/main/java/com/exercise/groupexpensemanagement/data/api/ApiService.java
+++ app/src/main/java/com/exercise/groupexpensemanagement/data/api/ApiService.java
@@ -25,7 +25,7 @@ public interface ApiService {
     Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss").create();
 
     ApiService apiService = new Retrofit.Builder()
-            .baseUrl("http://192.168.50.101/qlct/api/")
+            .baseUrl("http://192.168.213.1/qlct/api/")
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
             .create(ApiService.class);
